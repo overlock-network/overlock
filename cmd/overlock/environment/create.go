286477ddccee86751c24a8c3af55f59f5a751d3f
Jumiga Cătălin <catalinjumiga4@gmail.com>
2025-07-28 12:51:36 +0300

    Implemented config file for setup Environment (#306)

--- cmd/overlock/environment/create.go
+++ cmd/overlock/environment/create.go
@@ -2,22 +2,56 @@ package environment
 
 import (
 	"context"
+	"os"
 
 	"github.com/web-seven/overlock/pkg/environment"
 	"go.uber.org/zap"
+	"gopkg.in/yaml.v2"
 )
 
 type createCmd struct {
-	Name         string `arg:"" requried:"" help:"Name of environment."`
-	HttpPort     int    `optional:"" short:"p" help:"Http host port for mapping" default:"80"`
-	HttpsPort    int    `optional:"" short:"s" help:"Https host port for mapping" default:"443"`
-	Context      string `optional:"" short:"c" help:"Kubernetes context where Environment will be created."`
-	Engine       string `optional:"" short:"e" help:"Specifies the Kubernetes engine to use for the runtime environment." default:"kind"`
-	EngineConfig string `optional:"" help:"Path to the configuration file for the engine. Currently supported for kind clusters."`
-	MountPath    string `optional:"" help:"Path for mount to /storage host directory. By default no mounts."`
+	Name   string `arg:"" required:"" help:"Name of environment."`
+	Config string `optional:"" help:"Path to the Overlock configuration file. Defaults to ./overlock.yaml if present."`
+	createOptions
+}
+
+type createOptions struct {
+	HttpPort       int      `optional:"" short:"p" help:"Http host port for mapping" default:"80"`
+	HttpsPort      int      `optional:"" short:"s" help:"Https host port for mapping" default:"443"`
+	Context        string   `optional:"" short:"c" help:"Kubernetes context where Environment will be created."`
+	Engine         string   `optional:"" short:"e" help:"Specifies the Kubernetes engine to use for the runtime environment." default:"kind"`
+	EngineConfig   string   `optional:"" help:"Path to the configuration file for the engine. Currently supported for kind clusters."`
+	MountPath      string   `optional:"" help:"Path for mount to /storage host directory. By default no mounts."`
+	Providers      []string `optional:"" help:"List of providers to apply to the environment."`
+	Configurations []string `optional:"" help:"List of configurations to apply to the environment."`
+	Functions      []string `optional:"" help:"List of functions to apply to the environment."`
 }
 
 func (c *createCmd) Run(ctx context.Context, logger *zap.SugaredLogger) error {
+	configPath := c.Config
+	userProvidedConfig := c.Config != ""
+
+	if !userProvidedConfig {
+		configPath = "./overlock.yaml"
+	}
+	cfg, err := loadConfig(configPath)
+	if err != nil {
+
+		if os.IsNotExist(err) {
+			if userProvidedConfig {
+				logger.Errorf("Configuration file not found at specified path: %s", configPath)
+				return err
+			}
+		} else {
+			logger.Errorf("Failed to load or parse configuration file %s: %v", configPath, err)
+			return err
+		}
+	}
+
+	if cfg != nil {
+		c.createOptions = *cfg
+	}
+
 	return environment.
 		New(c.Engine, c.Name).
 		WithHttpPort(c.HttpPort).
@@ -25,5 +59,23 @@ func (c *createCmd) Run(ctx context.Context, logger *zap.SugaredLogger) error {
 		WithContext(c.Context).
 		WithMountPath(c.MountPath).
 		WithEngineConfig(c.EngineConfig).
+		WithProviders(c.Providers).
+		WithConfigurations(c.Configurations).
+		WithFunctions(c.Functions).
 		Create(ctx, logger)
+}
+
+func loadConfig(path string) (*createOptions, error) {
+	var cfg createOptions
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
